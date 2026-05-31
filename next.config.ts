8677b9ed0d013d9b936532412adec153b6275519
Eileen Kang <eileen_kang@tech.gov.sg>
2026-05-31 10:11:56 +0800

    fix: fix csp not allow loading of google font

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const cspDirectives = [
   "default-src 'self'",
   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://cdnjs.buymeacoffee.com",
-  "style-src 'self' 'unsafe-inline'",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: blob: https://*.supabase.co https://lh3.googleusercontent.com https://maps.gstatic.com https://maps.googleapis.com https://*.buymeacoffee.com",
-  "font-src 'self'",
+  "font-src 'self' https://fonts.gstatic.com",
   "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://maps.googleapis.com",
   "frame-src 'self' https://maps.googleapis.com",
   "object-src 'none'",
